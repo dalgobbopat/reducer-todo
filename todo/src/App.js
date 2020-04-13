ee9f82c6d8cadf0ba03a98fb9bb78e3f03116efd
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-04-13 18:22:39 -0400

    MVP and a bit of styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,11 @@
 import React, { useReducer } from 'react';
 import './App.css';
 import { initialState, reducer} from './Reducers/reducer';
+import { TodoForm } from './components/todoForm';
+import { TodoList } from './components/todoList';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initailState)
+  const [state, dispatch] = useReducer(reducer, initialState)
   // console.log(state)
   const addTodo = (input) => {
     const newTodo = {
@@ -21,10 +23,16 @@ function App() {
   const clearCompleted = () => {
     dispatch({type: 'CLEAR_COMPLETED_TODO'})
   }
-  
+
   return (
     <div className="App">
-    <h1>Todo</h1>
+        <h1>To Do</h1>
+        <TodoForm addTodo={addTodo} />
+        <TodoList state={state} handleComplete={handleComplete} />
+        <button onClick={(e) => {
+            e.preventDefault()
+            clearCompleted()
+        }}>Clear</button>
     </div>
   );
 }
